97f9b66e2c9be348df685e6ecedfd51d4ae577c4
Alessandro Visotto <visotto@users.noreply.github.com>
2014-06-27 02:12:55 -0300

    Alterações no main.cpp

--- main.cpp
+++ main.cpp
@@ -18,16 +18,50 @@ void gerenciaMain(Sessao **s)
     cout << "Escolha uma opcao:" << endl;
     cout << "0. Sair" << endl;
     cout << "1. Inserir sessao" << endl;
+    cout << "2. Inserir sessao" << endl;
+    cout << "3. Inserir sessao" << endl;
+    cout << "1. Inserir sessao" << endl;
     cin >> opcao_externa;
 
     if (!opcao_externa) return;
 
     switch (opcao_externa)
     {
-      case 1:
+      case 1:   //Boundary ou control do Gerencia Sala
         cout << "Digite o"
-        switch (opcao_interna)
-    }
+        break;
+        switch (opcao_interna){
+          case 1:
+          break;
+        }
+      break;
+
+      case 2:   //Boundary ou control do Sessao Sala
+      cout << "Digite o"
+        break;
+        switch (opcao_interna){
+          case 1:
+          break;
+        }
+      break;
+
+      case 3:   //Boundary ou control do Filme Sala
+      cout << "Digite o"
+        break;
+        switch (opcao_interna){
+          case 1:
+          break;
+        }
+      break;
+
+      case 4:   //Boundary ou control do Venda Sala
+      cout << "Digite o"
+        break;
+        switch (opcao_interna){
+          case 1:
+          break;
+        }
+      break;
   }
 }
 
